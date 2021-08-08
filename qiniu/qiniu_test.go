5e8b850c01c6fa8bca8766972b5c745b34621a26
taoey <hwt8080@163.com>
2021-08-08 22:03:19 +0800

    更新测试文件

--- qiniu/qiniu_test.go
+++ qiniu/qiniu_test.go
@@ -3,7 +3,7 @@ package qiniu
 import "testing"
 
 func TestUploadToQiniu(t *testing.T) {
-	filepath := "/Users/tao/Documents/workspace/go/gomod/MdHelper/README.assets/image-20210808202426899.png"
+	filepath := "./../README.assets/image-20210808202426899.png"
 	filename := "github-x.png"
 
 	qiniuClient := Qiniu{
